f3565dcbe20bab679d8548e344f70dc15f2cc60b
Mallku2 <mallkuernesto@gmail.com>
2016-04-13 19:17:47 -0300

    Abstracting functionality...

---
+++ clustering/spider_utilities/common_utilities.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+from selectors import sites_selectors, title_selector_index, body_selector_index
+import re
+import scrapy
+
+# TODO: iba a colocar aqui los metodos de la arana que se encargan de parsear
+# el cuerpo de las noticias, pero me parece que esta mezclado con la logica
+# propia de lo que hace aquella arana...
+def extract_element(response, selector_index):
+    ret = None
+    selectors = response.meta[selector_index]
+    # Try with different selectors
+    for selector in selectors:
+        selector_result = response.xpath(selector).extract()
+        if len(selector_result) == 1:
+            element_html = selector_result[0]
+            element_parser = BeautifulSoup(element_html, 'html.parser')
+            # Delete scripts
+            for script in element_parser(["script", "style"]):
+                script.extract()
+
+            ret = element_parser.get_text()
+            break
+
+    return ret
+
+def generate_appr_request(link, callback):
+    """Analizes a string representing a link to a news' site,
+        generates a request to that link and selects the apropiate callback
+        method to handle the response. Also, saves the article into the
+        corpus.
+
+        PARAMS
+        link: a string representing the link to the site.
+        callback: the callback procedure that will receive the response
+                to the request."""
+    # TODO: re: determinar si esta forma de hacerlo es eficiente.
+    req = None
+    for site_link, selectors in sites_selectors.iteritems():
+        site_reg_exp = ".*" + site_link + ".*"
+        match = re.search(site_reg_exp, link)
+        if match:
+            req = scrapy.Request(link,
+                                callback=callback)
+            req.meta["title_selector"] = selectors[title_selector_index]
+            req.meta["body_selector"] = selectors[body_selector_index]
+            break
+
+    # TODO: fijarse cuando debo devolver return, y cuando yield.
+    return req
---
+++ clustering/spider_utilities/selectors.py
@@ -0,0 +1,55 @@
+title_selector_index = "title_selector"
+body_selector_index = "body_selector"
+rss_index = "rss_index"
+# TODO: posiblemente tengamos que agregar aqui el link al rss de cada portal
+google_news_main_selectors = {"http://news.google.com/news/story" : {
+                "news_selector" : 
+                    "//div[@class=\"story-page-main\"]//div[@class=\"topic\"]"
+                                                                    }
+                            }
+sites_selectors = {"www\.infobae\.com" :
+                          {title_selector_index : ["//header[@class=\"detail-header clearfix\"]/h1[@class=\"entry-title\"]"],
+                          body_selector_index : ["//div[@class=\"cuerposmart clearfix\"]/div"],
+                          rss_index : "http://cdn01.ib.infobae.com/adjuntos/162/rss/Infobae.xml"},
+
+                    "www\.clarin\.com" :
+                      {title_selector_index : ["//div[@class=\"int-nota-title\"]/h1"],
+                      body_selector_index : ["//div[@class=\"nota\"]"],
+                      rss_index : "http://www.clarin.com/rss/lo-ultimo/"},
+
+                    "www\.pagina12\.com\.ar" :
+                      {title_selector_index : ["//div[@class=\"nota ultima-noticia top12\"]/h2",
+                                                "//div[@class=\"nota top12\"]/h2"],
+                      body_selector_index : ["//div[@id=\"cuerpo\"]"],
+                      rss_index : "http://www.pagina12.com.ar/diario/rss/ultimas_noticias.xml"},
+
+                    "www\.lavoz\.com\.ar" :
+                      {title_selector_index : ["//header[@class=\"Main\"]/h1"],
+                      body_selector_index : ["//div[@class=\"field-item even\"]"],
+                      rss_index : "http://www.lavoz.com.ar/rss.xml"},
+
+                    "www\.lanacion\.com\.ar" :
+                      {title_selector_index : ["//h1[@itemprop=\"headline\"]"],
+                      body_selector_index : ["//section[@id=\"cuerpo\"]"],
+                      rss_index : "http://contenidos.lanacion.com.ar/herramientas/rss-origen=2"},
+
+                    "www\.telam\.com\.ar" :
+                    {title_selector_index : ["//div[@class=\"title\"]/h2"],
+                    body_selector_index : ["//div[@class=\"main-content\"]/div[@class=\"editable-content\"]"],
+                    rss_index : "http://www.telam.com.ar/rss2/ultimasnoticias.xml"},
+
+                    "www\.cadena3\.com" :
+                    {title_selector_index : ["//div[@id=\"nota-ampliada\"]/div[@class=\"titulo\"]"],
+                    body_selector_index : ["//div[@id=\"nota-ampliada\"]/div[@class=\"cuerpo\"]"],
+                    rss_index : "http://cadena3.com/rss/Internacionales.xml"},
+
+                    "www\.perfil\.com" :
+                    {title_selector_index : ["//div[@class=\"contenido\"]//header[@id=\"header-noticia\"]/hgroup"],
+                    body_selector_index : ["//div[@class=\"contenido\"]//div[@itemprop=\"articleBody\"]"],
+                    rss_index : "http://www.perfil.com/rss/ultimomomento.xml"},
+
+                    "www\.ambito\.com" :
+                    {title_selector_index : ["//div[@id=\"contenido\"]//h2[@id=\"tituloDespliegue\"]"],
+                    body_selector_index : ["//div[@id=\"contenido\"]//div[@id=\"textoDespliegue\"]",
+                                            "//div[@id=\"contenido\"]//p[@id=\"textoDespliegue\"]"],
+                    rss_index : "http://www.ambito.com/rss/noticiasp.asp"}}
