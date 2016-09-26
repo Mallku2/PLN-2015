c90717f93f605b88f2893c766cbec7ec0ae54f56
Mallku2 <mallkuernesto@gmail.com>
2016-09-26 12:47:26 -0300

    "Threaded" version of the clustering algorithm.

--- clustering/clustering/clustering_component.py
+++ clustering/clustering/clustering_component.py
@@ -1,16 +1,24 @@
 # -*- coding: utf-8 -*-
+from threading import Thread
 from scipy.sparse import dok_matrix
 import pickle
+import time
+import sys
 from math import sqrt
+from clustering.bag_of_words_rep.feature_selection import VectRep
 
 
 # TODO: hara falta tambien guardar aquí la longitud del vector?
+# TODO: vamos a tener que agregar mas informcion en este objeto: url del rtic
 class Document(object):
-    def __init__(self, text, vec_rep, vec_rep_length):
-        assert(isinstance(vec_rep, dok_matrix))
+    def __init__(self, url, text, vec_rep):
+        assert(isinstance(url, str))
+        assert(isinstance(text, str))
+        assert(isinstance(vec_rep, VectRep))
+
+        self._url = url
         self._text = text
         self._vec_rep = vec_rep
-        self._vec_rep_length = vec_rep_length
 
     def get_text(self):
         return self._text
@@ -19,26 +27,70 @@ class Document(object):
         return self._vec_rep
 
     def get_vec_rep_length(self):
-        return self._vec_rep_length
+        return self._vec_rep.get_length()
+
+    def get_url(self):
+        return self._url
 
 
 class Cluster(object):
     """Cluster of documents, with their centroid.
     """
-    def __init__(self, cent_text, cent_vec_rep, cent_length, cluster_id):
+    # TODO: la url la agrego por que es mi form de identificar y comparar cada
+    # documento
+    def __init__(self, url_cent, cent_text, cent_vec_rep, cluster_id):
         # TODO: me hace falta guardar el texto del centroide?
-        assert(isinstance(cent_vec_rep, dok_matrix))
+        assert(isinstance(cent_vec_rep, VectRep))
+        self._cluster_id = cluster_id
         # TODO: agrego el cluster_id, para facilitar el debugging. Pero dejo
         # que desde el exterior se decida la forma de determinar el id
-        self._cluster_id = cluster_id
-        centroid = Document(cent_text, cent_vec_rep, cent_length)
-        self._centroid = centroid
-        self._documents = set()
-        self._documents.add(centroid)
+        self._centroid = cent_vec_rep
+        self._documents = [Document(url_cent, cent_text, cent_vec_rep)]
 
-    def add_document(self, document, doc_vec_rep, len_vec_rep):
-        self._documents.add(Document(document, doc_vec_rep, len_vec_rep))
-        self._update_centroid()
+    def add_document(self, url_doc, document, doc_vec_rep):
+        self._documents.append(Document(url_doc, document, doc_vec_rep))
+        # TODO: es eficiencte que siempre que agregue un nuevo documento,
+        # me ponga a actualizar el centroide?.
+        self._update_centroid(doc_vec_rep)
+        # TODO: debugging
+        self._check_property()
+
+    # TODO: debugging
+    def _cosine_similarity(self, vect_1, vect_2):
+        """Returns the cosine similarity between vect1 and vect2.
+        PARAMS
+        vect_1 : instance of VecrRep.
+        vect_2 : instance of VectRep."""
+
+        l_vect_1 = vect_1.get_length()
+        l_vect_2 = vect_2.get_length()
+
+        assert(l_vect_1 > 0.0 and l_vect_2 > 0.0)
+
+        dot_product = 0
+
+        # Get the positions of the non-zero entries of vect_1
+        if vect_1.get_dimensions() <= vect_2.get_dimensions():
+            cols = vect_1.nonzero()
+        else:
+            # {vect_1.get_dimensions() > vect_2.get_dimensions()}
+            cols = vect_2.nonzero()
+
+        for pos in cols:
+            dot_product += vect_1[pos] * vect_2[pos]
+
+        return dot_product / (l_vect_1 * l_vect_2)
+
+    # TODO: debugging
+    def _check_property(self):
+        for doc_1 in self._documents:
+            for doc_2 in self._documents:
+                if id(doc_1) != id(doc_2):
+                    assert(self._cosine_similarity(doc_1.get_vec_rep(),
+                           doc_2.get_vec_rep()) >= -0.68)
+                    # TODO: debugging!
+                    # sys.stdout.write("\rProperty passed!")
+                    # sys.stdout.flush()
 
     def get_documents(self):
         return self._documents
@@ -46,61 +98,95 @@ class Cluster(object):
     def get_centroid(self):
         return self._centroid
 
-    def get_size(self):
-        return len(self._documents)
-
     def get_cluster_id(self):
         return self._cluster_id
+
+    def get_size(self):
+        return len(self._documents)
 
     # TODO: hace realmente falta este método, ahora que agregué un método para
     # alterar el centroide?
     def set_documents(self, documents):
         self._documents = documents
 
-    # TODO: parece que ya no hace falta este método
-    def set_centroid(self, cent_vect_rep, cent_length):
-        self._centroid = Document("", cent_vec_rep, cent_length)
-
-    def is_in_cluster(self, doc_vec_rep):
+    # TODO: esto me parece que lo uso solo para tests...?
+    def is_in_cluster_by_vector(self, doc_vec_rep):
         # TODO: solo me interesa preguntar por la representación vectorial?
-        ret = None
+        ret = False
 
         for doc_in_clstr in self._documents:
-            result = (doc_in_clstr.get_vec_rep() - doc_vec_rep)
-            col, rows = result.nonzero()
-            ret = len(col) == 0
-
+            ret = doc_vec_rep.is_equal_to(doc_in_clstr.get_vec_rep())
             if ret:
                 break
 
         return ret
 
-    def _update_centroid(self):
-        new_centroid = dok_matrix((1, 1))
+    def is_in_cluster_by_url(self, news_url):
+        assert(isinstance(news_url, str))
+        ret = False
+
+        for doc_in_clstr in self._documents:
+            ret = doc_in_clstr.get_url() == news_url
+            if ret:
+                break
+
+        return ret
+
+    def _update_centroid(self, new_vect):
+        dim_new_vect = new_vect.get_dimensions()
+        if dim_new_vect > self._centroid.get_dimensions():
+            # TODO: si esto ocurre, no hay problema?
+            self._centroid.resize(dim_new_vect)
+
+        centroid_comp_sum = pow(self._centroid.get_length(), 2.0)
+        size_cluster = float(self.get_size())
+        cols = new_vect.nonzero()
+        for pos in cols:
+            centroid_comp_sum -= pow(self._centroid[pos], 2.0)
+            self._centroid[pos] *= size_cluster
+            self._centroid[pos] += new_vect[pos] / size_cluster
+            centroid_comp_sum += pow(self._centroid[pos], 2.0)
+
+        """cols = self._centroid.nonzero()
+        sum_weights = 0.0
+        for pos in cols:
+            sum_weights += pow(self._centroid[pos], 2.0)"""
+
+        # TODO: hace realmente falta tomar la raiz cuadrada?
+        self._centroid.set_length(sqrt(centroid_comp_sum))
+
+
+
+    """def _update_centroid(self):
+        new_centroid = VectRep(1)
         centroid_dimensions = 1
         first_doc = True  # Are we analyzing the first document?
         for doc in self._documents:
             vec_rep = doc.get_vec_rep()
-            dimensions = vec_rep.shape[1]
+            dimensions = vec_rep.get_dimensions()
             if dimensions > centroid_dimensions:
                 # TODO: si esto ocurre, no hay problema?
-                new_centroid.resize(vec_rep.shape)
+                new_centroid.resize(dimensions)
                 centroid_dimensions = dimensions
 
-            rows, cols = vec_rep.nonzero()
+            cols = vec_rep.nonzero()
 
-            for pos in zip(rows, cols):
+            for pos in cols:
                 new_centroid[pos] += vec_rep[pos]
 
-        rows, cols = new_centroid.nonzero()
+        cols = new_centroid.nonzero()
         size_cluster = float(self.get_size())
         sum_weights = 0.0
-        for pos in zip(rows, cols):
+        for pos in cols:
             new_centroid[pos] /= size_cluster
             sum_weights += pow(new_centroid[pos], 2.0)
 
         # TODO: hace realmente falta tomar la raiz cuadrada?
-        self._centroid = Document("", new_centroid, sqrt(sum_weights))
+        new_centroid.set_length(sqrt(sum_weights))
+        # TODO: me parece que esto me esta mostrando que no necesito
+        # para nada que _centroid sea una instancia de Document.
+        # Me basta con que sea un VectRep...
+        self._centroid = new_centroid"""
 
     # TODO: revisar si esta es la mejor form de implementar la iteración sobre
     # los documentos
@@ -114,11 +200,18 @@ class Cluster(object):
 
 class Clusters(object):
     # TODO: debería hacerlo iterativo esto????
-    def __init__(self, data_file):
-        self._data_file = open(data_file, "r+b")
+    def __init__(self, data_file_name):
+        self._log_file = open("ClusterLog", "a")
+        self._log_file.write("Abriendo ClusterLog\n")
+        self._data_file = open(data_file_name, "r+b")
+
         # Determine if the file contains data.
-        self._data_file.seek(0, 2)  # Move file's pointer to its end.
-        if self._data_file.tell() > 0:
+        # Move the file's pointer 0 bytes from the end of the stream
+        # (whence == 2): move file's pointer to the end of the file.
+        # Returns the new position of the file's pointer.
+        if self._data_file.seek(0, 2) > 0:
+            self._log_file.write("data_tests tenia informacion\n")
+            # The file has data. Return the file's pointer to the beginning.
             self._data_file.seek(0)
             # There should be a dictionary and a set, saved into _data_file.
             # Dictionary of the form:
@@ -133,7 +226,8 @@ class Clusters(object):
             self._data_file.seek(0)
             self._data_file.truncate()
         else:
-            # {_data_file.tell() == 0}
+            # {self._data_file.seek(0, 2) == 0}
+            self._log_file.write("data_tests NO tenia informacion\n")
             self._clusters = {}
             self._scraped_news = set()
             self._time_last_article_added = {}
@@ -141,23 +235,35 @@ class Clusters(object):
     def get_cluster(self, cluster_id):
         return self._clusters[cluster_id]
 
-    def add_document(self, cluster_id, doc_vec_rep, len_vec_rep, document):
-        self._clusters[cluster_id].add_document(document, doc_vec_rep,
-                                                len_vec_rep)
-        new_centroid = self._clusters[cluster_id].get_centroid()
+    def add_document(self, cluster_id, url_doc, doc_vec_rep,
+                     document):
+        self._clusters[cluster_id].add_document(url_doc, document, doc_vec_rep)
+        # TODO: realmente me hace falta cambiar el id?
+        """new_centroid = self._clusters[cluster_id].get_centroid()
         # TODO: alguna razón para utilizar el id de un centroide como clave?
         self._clusters[id(new_centroid)] = self._clusters[cluster_id]
-        self._clusters.pop(cluster_id)
+        self._clusters.pop(cluster_id)"""
 
-    def add_new_cluster(self, document, doc_vec_rep, l_document):
-        assert(isinstance(doc_vec_rep, dok_matrix))
+    def add_new_cluster(self, url_doc, document, doc_vec_rep):
+        assert(isinstance(doc_vec_rep, VectRep))
+        self._clusters[url_doc] = Cluster(url_doc, document, doc_vec_rep,
+                                          url_doc)
 
-        cluster_id = id(doc_vec_rep)
-        self._clusters[cluster_id] = Cluster(document, doc_vec_rep,
-                                             l_document, cluster_id)
-
-    def get_length(self):
+    def get_clusters_quantity(self):
         return len(self._clusters)
+
+    def save_clusters(self):
+        """NOTE: from
+        https://docs.python.org/3/reference/datamodel.html: "It is not
+        guaranteed that __del__() methods are called for objects that still
+        exist when the interpreter exits"
+        """
+        pickle.dump(self._clusters, self._data_file)
+        # TODO: al picklear este set salta el SEGFAULT!
+        pickle.dump(self._scraped_news, self._data_file)
+        pickle.dump(self._time_last_article_added, self._data_file)
+        self._data_file.close()
+        self._log_file.close()
 
     # TODO: revisar si esta es la mejor form de implementar la iteración sobre
     # Clusters
@@ -168,45 +274,34 @@ class Clusters(object):
     def __next__(self):
         return next(self._clusters_iterator)
 
-    def __del__(self):
-        pickle.dump(self._clusters, self._data_file)
-        pickle.dump(self._scraped_news, self._data_file)
-        pickle.dump(self._time_last_article_added, self._data_file)
-        self._data_file.close()
-
 
 class KMeansClusteringComponent(object):
 
-    def __init__(self, data_file):
-        self._clusters = Clusters(data_file)
+    def __init__(self, data_file_name, clustering_threshold):
+        self._log_file = open("ClustersLog", "a")
+        self._log_file.write("Abriendo ClustersLog\n")
+        self._clusters = Clusters(data_file_name)
+        self._log_file.write("Instancia De Clusters creada\n")
         # TODO: testear para determinar este valor
-        self._clustering_threshold = 0.4
+        self._clustering_threshold = clustering_threshold
         # TODO: a continuacion deberiamos ubicar los primeros k-centroides?
 
-    def add_document(self, document, doc_vec_rep, len_vec_rep):
+    def add_document(self, url_doc, document, doc_vec_rep):
         # TODO: por que no realizo aquí la construcción de la representación
         # vectorial?
         """Applies the k-means algorithm to cluster the given document.
 
             PARAMS
-            doc_vec_rep: dok_matrix, bag-of-words representation of an article.
+            doc_vec_rep: VectRep, bag-of-words representation of an article.
         """
         nrst_centroid = None
         dist_nrst_centroid = float("-inf")
+        l_document = doc_vec_rep.get_length()
 
-        rows, cols = doc_vec_rep.nonzero()
-        summatory = 0.0
-        # TODO: esto se puede escribir en una sola linea
-        for pos in zip(rows, cols):
-            summatory += pow(doc_vec_rep[pos], 2.0)
-        # TODO: hace falta calcular sqrt?
-        l_document = sqrt(summatory)
         for cluster in self._clusters:
             centroid = cluster.get_centroid()
             # Calculate the distance between the centroid and the document
-            dist = self._cosine_similarity(centroid.get_vec_rep(),
-                                           centroid.get_vec_rep_length(),
-                                           doc_vec_rep, l_document)
+            dist = self._cosine_similarity(centroid, doc_vec_rep)
 
             if dist > dist_nrst_centroid:
                 dist_nrst_centroid = dist
@@ -219,14 +314,14 @@ class KMeansClusteringComponent(object):
             # el centroide de este cluster.
 
             # As we added a new document, we must redefine its centroid
-            self._clusters.add_document(nrst_centroid, doc_vec_rep, l_document,
+            self._clusters.add_document(nrst_centroid, url_doc, doc_vec_rep,
                                         document)
 
         else:
             # {dist_nrst_centroid < self._clustering_threshold}
             # The nearest centroid is further than the min. threshold.
             # Create a new cluster, with "document" as its centroid
-            self._clusters.add_new_cluster(document, doc_vec_rep, l_document)
+            self._clusters.add_new_cluster(url_doc, document, doc_vec_rep)
 
     def get_clusters(self):
         return self._clusters
@@ -238,21 +333,80 @@ class KMeansClusteringComponent(object):
         return self._scraped_news
 
     def get_clusters_quantity(self):
-        return self._clusters.get_length()
+        return self._clusters.get_clusters_quantity()
 
-    def _cosine_similarity(self, vect_1, l_vect_1, vect_2, l_vect_2):
+    def save_clusters(self):
+        self._clusters.save_clusters()
+
+    def get_cluster_from_news_url(self, news_url):
+        assert(isinstance(news_url, str))
+        ret = None
+
+        for cluster in self._clusters:
+            if cluster.is_in_cluster_by_url(news_url):
+                ret = cluster
+                break
+
+        return ret
+
+    def _cosine_similarity(self, vect_1, vect_2):
         """Returns the cosine similarity between vect1 and vect2.
         PARAMS
-        vect_1 : instance of scipy.sparse.dok_matrix, of 1xn dimensions.
-        vect_2 : instance of scipy.sparse.dok_matrix, of 1xn dimensions.
-        l_vect_1 : length of vect_1
-        l_vect_2 : length of vect_2"""
+        vect_1 : instance of VecrRep.
+        vect_2 : instance of VectRep."""
+
+        l_vect_1 = vect_1.get_length()
+        l_vect_2 = vect_2.get_length()
+
+        assert(l_vect_1 > 0.0 and l_vect_2 > 0.0)
+
         dot_product = 0
 
         # Get the positions of the non-zero entries of vect_1
-        rows, cols = vect_1.nonzero()
+        if vect_1.get_dimensions() <= vect_2.get_dimensions():
+            cols = vect_1.nonzero()
+        else:
+            # {vect_1.get_dimensions() > vect_2.get_dimensions()}
+            cols = vect_2.nonzero()
 
-        for pos in zip(rows, cols):
+        for pos in cols:
             dot_product += vect_1[pos] * vect_2[pos]
 
         return dot_product / (l_vect_1 * l_vect_2)
+
+
+# TODO: primera implementación de un hilo consumidor de vectores, desde
+# una cola thread-safe
+class ThreadedKMeansClusteringComponent(Thread):
+    def __init__(self, queue, data_file_name, sentinel_value):
+        """
+        PARAMS
+
+        queue : an instance of Queue
+                (https://docs.python.org/3.5/library/queue.html)
+        """
+        self._queue = queue
+        self._clustering_component = KMeansClusteringComponent(data_file_name)
+        self._articles_available = True
+        self._sentinel_value = sentinel_value
+        Thread.__init__(self)
+
+    def run(self):
+        # TODO: este mecanismo de corte es dependiente del tipo de test
+        # que estoy realizando...
+        articles_processed = 1
+        while self._articles_available:
+            # Block until an element is available in the queue
+            url_article, text, vect_rep = self._queue.get(block=True)
+
+            # Check against the sentinel value
+            if url_article != self._sentinel_value:
+                self._clustering_component.add_document(url_article,
+                                                        text,
+                                                        vect_rep)
+                sys.stdout.write("\rClustering - articles processed:%d" % articles_processed)
+                sys.stdout.flush()
+                articles_processed += 1
+            else:
+                # {url_article == self._sentinel_value}
+                self._articles_available = False
